e71ef4b77d40eb4557b55f11830e36d21de6acca
laujamie <36782341+laujamie@users.noreply.github.com>
2021-03-06 07:27:16 +0000

    Add middleware to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,29 @@
 import express from 'express';
 import { Model } from 'objection';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import db from './db';
 
 Model.knex(db);
 
+// Config variables
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Set up gkobal app middleware
+const corsConfig = {
+  origin: [process.env.CLIENT_URL || 'http://localhost:3000'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsConfig));
+app.use(helmet());
+app.use(morgan('common'));
+app.use(express.json());
+
+// Routers and routes
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
