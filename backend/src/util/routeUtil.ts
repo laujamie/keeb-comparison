37db5db2f49f89a9091d1e1a406d01bbc279a4f1
laujamie <36782341+laujamie@users.noreply.github.com>
2021-05-29 03:40:08 +0000

    Change types for route wrapper until typings are fixed

--- backend/src/util/routeUtil.ts
+++ backend/src/util/routeUtil.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction, Handler } from 'express';
 
-export const routeHandler = (fn: Handler): Handler => {
+// TODO: Fix this type issue
+export const routeHandler = (fn: any): Handler => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       return await fn(req, res, next);
